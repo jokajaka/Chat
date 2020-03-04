2f6c8d404c379d41ce6507cf36f1485138f99bef
jokajaka <danil777913@gmail.com>
2020-03-04 04:59:51 +0300

    Добавьте файлы проекта.

---
+++ Pulicoo/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Pulicoo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pulicoo.Hubs
+{
+	public class ChatHub : Hub
+	{
+		public async Task SendMessage(Message message) =>
+			await Clients.All.SendAsync("receiveMessage", message);
+	}
+}
